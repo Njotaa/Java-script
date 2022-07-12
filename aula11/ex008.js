499684802bd0d115ba76191ee193548329e6c390
Njotaa <105859260+Njotaa@users.noreply.github.com>
2022-07-12 14:44:28 +0100

    js condiçoes

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+//console.log('O console funcionou corretamente')
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel >60)
+    {// condição simples!
+        console.log('Ultapassou a velocidade permitida. MULTADO!')
+    }
+    console.log('Conduza sempre usando o cinto de segurança')
+
+
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var país = 'Espanha'
+console.log(`Vivendo em ${país}`)
+if(país !='Portugal') //se for diferente vai executar o primeiro console
+
+{console.log('Tu és estrangeiro')}
+
+
+else{
+    console.log('tu és Português')}
