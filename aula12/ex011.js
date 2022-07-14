ea82aef3d93125b34b202d9b5dd2f69d34297f29
Njotaa <105859260+Njotaa@users.noreply.github.com>
2022-07-14 01:45:09 +0100

    s

---
+++ aula12/ex011.js
@@ -0,0 +1,12 @@
+var idade =20
+console.log(`Voçê tem ${idade} anos.`)
+if(idade< 16)
+{
+    console.log('Não vota')
+}
+else if (idade<18){
+    console.log('voto opcional')
+}
+else{
+    console.log('voto obrigatório')
+}
