0a0d4163b6b4c3ed5c75818f3478faa3e04e25b9
Njotaa <105859260+Njotaa@users.noreply.github.com>
2022-07-18 12:51:26 +0100

    exercicio 2 repetiçôes

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,13 +1,14 @@
 /*Repetições */
 var c = 1
-while (c < 6) {
+while (c <= 6) {
 console.log('Tudo bem')
-//console.log(`Passo ${c}`)
+c++}
+/*console.log(`Passo ${c}`)
 c++ 
- // c=c+1 while quer dizer enquanto
+c=c+1 while quer dizer enquanto*/
  
 
- do {
-    console.log(`Passo ${c}´)
+ /*do {
+    console.log(`Passo ${c}`)
     c++
- } while(c <= 6)
+ } while(c <= 6)*/
